923c82a791d0179ed717cdb625e34a4ebb6bdf9a
0aubsq <0aubsq@gmail.com>
2021-01-21 07:38:40 +0100

    Restart button, add TMP and textures, add japanese font asset

---
+++ Assets/Scripts/AudioSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSingleton : MonoBehaviour
+{
+	static AudioSingleton instance = null;
+ 
+	void Awake()
+	{
+		if (instance != null) {
+			Destroy(gameObject);
+		}
+		else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/ReplayButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ReplayButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData) {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
