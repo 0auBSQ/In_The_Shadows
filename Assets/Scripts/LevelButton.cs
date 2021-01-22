5c8d39a75af789668f070cd55d917b68a0501ba9
0aubsq <0aubsq@gmail.com>
2021-01-22 07:07:54 +0100

    Return button, improved level select menu

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour
+{
+	public string level_scene;
+	public int level_index;
+	public int[] level_requirements;
+	private bool avaliable = true;
+
+    void Start()
+    {
+        foreach (int lr in level_requirements) {
+			if (Master.GetM.cleared_levels[lr] == 0) {
+				avaliable = false;
+			}
+		}
+		if (avaliable == false) {
+			GetComponent<Image>().color = new Color(0.6f, 0.6f, 0.6f, 1f);
+		}
+		GetComponent<Button>().onClick.AddListener(ClickEvent);
+    }
+	
+	void ClickEvent() {
+		print("cliqued");
+		if (avaliable == true) {
+			SceneManager.LoadScene(level_scene);
+		}
+	}
+	
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ReturnButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ReturnButton : MonoBehaviour, IPointerClickHandler
+{
+	public void OnPointerClick(PointerEventData eventData) {
+		SceneManager.LoadScene("LevelSelect");
+	}
+}
